ee0f65c7cb9ffe4119c51986202519f45831b7f8
antbrogame-a11y <antbrogame@gmail.com>
2025-09-30 13:04:05 +0200

    Aggiunto il file DragDropSchedule.js con la logica base per il drag & drop.

---
+++ components/DragDropSchedule.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function DragDropSchedule({ schedule, onUpdate }) {
+  // Qui si implementerà la logica drag & drop per l'orario
+  return (
+    <View>
+      <Text>Drag & Drop Orario (work in progress)</Text>
+      {/* TODO: implementa drag & drop */}
+    </View>
+  );
+}
